46fe6d43b8843a0f675f030f4d61b43790e0a11a
SunkaraboinaPraveenKumar <160092930+SunkaraboinaPraveenKumar@users.noreply.github.com>
2025-03-22 11:20:59 +0530

    Update app.py

--- AI_Blog_Writer/app.py
+++ AI_Blog_Writer/app.py
@@ -7,7 +7,7 @@ topic = st.text_input("Enter the Blog Topic:")
 
 if st.button("Generate Blog"):
     with st.spinner("Generating Blog...."):
-        response = requests.post("http://127.0.0.1:5000/generate",json={"topic":topic})
+        response = requests.post("https://agentic-projects-blog-writer-backend.onrender.com/generate",json={"topic":topic})
         if response.status_code==200:
             result = response.json()
             st.subheader("Generated Blog")
